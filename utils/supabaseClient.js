746319a941cd8a2a7a650a42d4fe6ce2db238520
Arnau Davis <arnau.davis@outlook.com>
2026-03-22 03:42:26 -0400

    removed vercel debugging

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,15 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
-
-console.log("CLIENT ENV CHECK:", {
-  url: supabaseUrl,
-  key: supabaseKey,
-})
-
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error("Missing Supabase environment variables at build time")
-}
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
+)
